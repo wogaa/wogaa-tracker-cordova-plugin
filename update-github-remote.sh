4dc01e3be55c7f9ff7087095d645c2dc98008fbf
Sachin Gamage <sachin.gamage@cognizant.com>
2024-12-11 07:09:39 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /update-github-remote.sh

--- update-github-remote.sh
+++ update-github-remote.sh
@@ -1,12 +1,15 @@
 PACKAGE_VERSION=$(node -p  "require('./package.json').version")
 git status
 
-if [ -z "$(git status --porcelain)" ]; then 
-  echo "No Changes detected for the version ${PACKAGE_VERSION} from the previous tag"
-else 
+git add . -- ':!.gitlab-ci.yml' ':!update-github-remote.sh'
+git commit -m "Updating github remote for ${PACKAGE_VERSION}"
+git push github master
 
-  git add . -- ':!.gitlab-ci.yml' ':!update-github-remote.sh'
-  git commit -m "Updating github remote for ${PACKAGE_VERSION}"
-  git push
-fi
+# if [ -z "$(git status --porcelain)" ]; then 
+#   echo "No Changes detected for the version ${PACKAGE_VERSION} from the previous tag"
+# else 
+#   git add . -- ':!.gitlab-ci.yml' ':!update-github-remote.sh'
+#   git commit -m "Updating github remote for ${PACKAGE_VERSION}"
+#   git push
+# fi
  
